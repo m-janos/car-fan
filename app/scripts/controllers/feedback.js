f2c9ec683308ffaaeeaf3d57bbc99a1ac6df4604
martin.janos <martin.janos@accenture.com>
2016-11-29 16:50:56 +0100

    Connected with email client

--- app/scripts/controllers/feedback.js
+++ app/scripts/controllers/feedback.js
@@ -9,7 +9,10 @@
  */
 angular.module('passatApp')
   .controller('FeedbackCtrl', ['$scope', '$location', function ($scope, $location) {
+      this.feedback = '';
       this.onSubmit = function($evt){
+        var body = this.feedback || '';
         alert("Your feedback has been processed successfully. Thank you!");
+        window.location.href = "mailto:user@example.com?subject=Subject&body="+body+"%20goes%20here";
       };
   }]);
